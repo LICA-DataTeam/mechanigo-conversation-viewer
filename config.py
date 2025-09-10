eccc74840d82b8bd5857d431f434577d6112a8eb
Val Subaan <valsubaan@yahoo.com>
2025-09-10 16:05:19 +0800

    Added secrets.toml (for prod)

--- config.py
+++ config.py
@@ -1,27 +1,21 @@
 from google.oauth2 import service_account
 from google.cloud import bigquery
 from dotenv import load_dotenv
-import json
-import os
+import streamlit as st
 
 load_dotenv()
 
-CREDS = os.getenv("CREDENTIALS")
+CREDS = st.secrets["gcp_service_account"]
 
 if not CREDS:
     raise ValueError("Missing Google Credentials!")
-
-try:
-    CREDS_FILE = json.loads(CREDS)
-except json.JSONDecodeError as e:
-    raise ValueError("Invalid JSON in the credentials env file") from e
 
 SCOPE = [
     'https://www.googleapis.com/auth/bigquery'
 ]
 
 GOOGLE_CREDS = service_account.Credentials.from_service_account_info(
-    CREDS_FILE,
+    CREDS,
     scopes=SCOPE
 )
 
